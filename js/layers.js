270ac9a0eeb88c8ef471a84294de3388bb45cdff
superguy8892 <superguy8892@gmail.com>
2025-03-10 20:19:10 -0500

    Changed color of Cat Centers
    basically just changed the color of the icon for the cat centers. lOl

--- js/layers.js
+++ js/layers.js
@@ -6,7 +6,7 @@ addLayer("c", {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDC13",
+    color: "##0000ff",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "cat centers", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
